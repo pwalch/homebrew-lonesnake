ae81460a49263b0a0151ad06d9bcd082004f1799
Pierre Walch <pwalch@gmail.com>
2022-01-04 13:51:04 +0100

    Version 0.4.0

--- standalone-venv.rb
+++ standalone-venv.rb
@@ -4,8 +4,8 @@
 class StandaloneVenv < Formula
   desc "tool to easily set up a self-contained Python environment with an interpreter and a venv in a '.standalone-venv' directory"
   homepage "https://github.com/pwalch/standalone-venv"
-  url "https://github.com/pwalch/standalone-venv/archive/refs/tags/0.3.0.tar.gz"
-  sha256 "290d637789010fe6cb04b4573779db7f493a3abbe8369c581f61c59e6468e2f7"
+  url "https://github.com/pwalch/standalone-venv/archive/refs/tags/0.4.0.tar.gz"
+  sha256 "7ac9abdb3495e0ee8193684d50190e6b7e2c54412a9b103da644d65f61b2f8fd"
   license "GPL-3.0-only"
 
   depends_on "openssl@3"
