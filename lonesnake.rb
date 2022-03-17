c4f0d804e12098f6eafa68b7b3619549c9c78cfa
Pierre Walch <code@pwal.ch>
2022-03-17 12:39:33 +0100

    Release 0.7.1

--- lonesnake.rb
+++ lonesnake.rb
@@ -4,8 +4,8 @@
 class Lonesnake < Formula
   desc "create self-contained Python environments with interpreter and venv in a single directory"
   homepage "https://github.com/pwalch/lonesnake"
-  url "https://github.com/pwalch/lonesnake/archive/refs/tags/0.7.0.tar.gz"
-  sha256 "28baa7d3ffc137bf78724872ee3e7e32b9742aa80d53e5738ba5f4cab544b0cb"
+  url "https://github.com/pwalch/lonesnake/archive/refs/tags/0.7.1.tar.gz"
+  sha256 "ccf76490f8de44cb7c82da490bdb3fa109f3038a8c32662dea4b8b6fc31487be"
   license "GPL-3.0-only"
 
   depends_on "openssl@3"
