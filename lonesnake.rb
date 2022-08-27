7f135651720f7db926827275a4e2bb2c2e4d81e1
Pierre Walch <code@pwal.ch>
2022-08-27 23:14:47 +0200

    Bump version number to 0.12.2

--- lonesnake.rb
+++ lonesnake.rb
@@ -4,8 +4,8 @@
 class Lonesnake < Formula
   desc "create self-contained Python environments with interpreter and venv in a single directory"
   homepage "https://github.com/pwalch/lonesnake"
-  url "https://github.com/pwalch/lonesnake/archive/refs/tags/0.12.1.tar.gz"
-  sha256 "892491f3f6c3b0e23aa5cb4980c9ddded64e6dfdb8eb7c093470720d76225423"
+  url "https://github.com/pwalch/lonesnake/archive/refs/tags/0.12.2.tar.gz"
+  sha256 "05e5ba1d559314e401f24b8017cae6bb38972b6e7113041a5dd5e80f0b097388"
   license "GPL-3.0-only"
 
   depends_on "openssl@3"
