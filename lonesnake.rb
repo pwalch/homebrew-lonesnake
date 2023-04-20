d11851322b6fe44bbec6d102e11b160e3f08fc4e
Pierre Walch <code@pwal.ch>
2023-04-21 00:11:27 +0200

    Update description to match companion repo

--- lonesnake.rb
+++ lonesnake.rb
@@ -2,7 +2,7 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Lonesnake < Formula
-  desc "create self-contained Python environments with interpreter and venv in a single directory"
+  desc "generate self-contained Python environments in a single directory with a single command"
   homepage "https://github.com/pwalch/lonesnake"
   url "https://github.com/pwalch/lonesnake/archive/refs/tags/0.20.0.tar.gz"
   sha256 "0c5dac4b105e3b936e5b7c3d3ec7f477ff867623bbb1bd3d68d7e514ea91d95d"
