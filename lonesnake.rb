678fbb179de6cc23d7dd66d26635e242b9b5bf04
Pierre Walch <code@pwal.ch>
2023-09-08 11:41:36 +0200

    Bump version number to 0.22.0

--- lonesnake.rb
+++ lonesnake.rb
@@ -2,10 +2,10 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Lonesnake < Formula
-  desc "generate self-contained Python environments in a single directory with a single command"
+  desc "self-contained Python environments with a single command"
   homepage "https://github.com/pwalch/lonesnake"
-  url "https://github.com/pwalch/lonesnake/archive/refs/tags/0.21.0.tar.gz"
-  sha256 "7fd93498d59c9c22788c2aca286aa34f562bd8e1923b102f145688191c2dbb52"
+  url "https://github.com/pwalch/lonesnake/archive/refs/tags/0.22.0.tar.gz"
+  sha256 "6d71fdb97733a03cabad5d5b6a97651a57b732f61c53a715ab535a20b212dce0"
   license "GPL-3.0-only"
 
   depends_on "openssl@3"
