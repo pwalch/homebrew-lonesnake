5a6a9a74060228dc7cf91c80d395ffdb4f3062cc
Pierre Walch <code@pwal.ch>
2023-12-17 23:20:42 +0100

    Bump version number to 0.27.0 and add lonesnake-kit formula

--- lonesnake-kit.rb
+++ lonesnake-kit.rb
@@ -1,20 +1,16 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Lonesnake < Formula
-  desc "self-contained Python environments with a single command"
+class LonesnakeKit < Formula
+  desc "configure development environments faster for lonesnake"
   homepage "https://github.com/pwalch/lonesnake"
   url "https://github.com/pwalch/lonesnake/archive/refs/tags/0.27.0.tar.gz"
   sha256 "98017d9860fb17b44e2240730b23970891d805b569ffe12b940daf3c86c04e6b"
   license "GPL-3.0-only"
 
-  depends_on "openssl@3"
-  depends_on "readline"
-  depends_on "sqlite"
-  depends_on "xz"
-  depends_on "zlib"
+  depends_on "pwalch/lonesnake/lonesnake"
 
   def install
-    bin.install "lonesnake"
+    bin.install "lonesnake-kit"
   end
 end
