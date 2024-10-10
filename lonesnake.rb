573ea5418be81d44129cd8041cc4ac3e782c53d3
Pierre Walch <code@pwal.ch>
2024-10-10 18:21:30 +0200

    Remove comments at top of formula file

--- lonesnake.rb
+++ lonesnake.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Lonesnake < Formula
   desc "self-contained Python environments with a single command"
   homepage "https://github.com/pwalch/lonesnake"
