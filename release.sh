71db6a4774f82712d889851b3dd698e24c0eddbd
Pierre Walch <code@pwal.ch>
2025-05-18 22:00:30 +0200

    Simplify release script to use release version number as input

--- release.sh
+++ release.sh
@@ -4,18 +4,12 @@ set -euo pipefail
 
 if [[ $# -ne 1 ]]; then
     echo "[ERROR] Invalid arguments" >&2
-    echo "Usage: $0 CURRENT_VERSION_NUMBER" >&2
+    echo "Usage: $0 RELEASE_VERSION_NUMBER" >&2
     exit 1
 fi
 
-current_version="$1"
-major_version=$(echo "$current_version" | cut -d. -f1)
-minor_version=$(echo "$current_version" | cut -d. -f2)
-patch_version=$(echo "$current_version" | cut -d. -f3)
-
-next_minor_version=$((minor_version + 1))
-new_version="${major_version}.${next_minor_version}.${patch_version}"
-new_version_filename="${new_version}.tar.gz"
+release_version="$1"
+new_version_filename="${release_version}.tar.gz"
 if ! curl -sL -I "https://github.com/pwalch/lonesnake/archive/refs/tags/${new_version_filename}"; then
     echo "[ERROR] Could not download lonesnake archive version ${new_version_filename}" >&2
     exit 1
@@ -28,11 +22,8 @@ fi
 
 echo "Archive checksum: ${archive_checksum}"
 
-escaped_current_version=$(echo "$current_version" | sed 's/\./\\./g')
-escaped_new_version=$(echo "$new_version" | sed 's/\./\\./g')
 echo "Update Brew formula with new version"
-echo "s/${escaped_current_version}/${escaped_new_version}/"
-if ! gsed -i "s/${escaped_current_version}/${escaped_new_version}/" lonesnake.rb; then
+if ! gsed -i "s|tags/.*.tar.gz|tags/$release_version.tar.gz|" lonesnake.rb; then
     echo "[ERROR] Could not replace version number in lonesnake.rb" >&2
     exit 1
 fi
